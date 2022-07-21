87e660ca2a91ba6d1625a71e3a5d10ef62be1a26
tranvandoan1 <tranlove24@gmail.com>
2022-07-21 16:20:32 +0700

    dá

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,78 +1,76 @@
-import User from '../modoles/Users';
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
+import User from "../modoles/Users";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 dotenv.config();
-const expressJwt = require('express-jwt');
-
+const expressJwt = require("express-jwt");
 
 export const signup = (req, res) => {
-
-    const user = new User(req.body);
-    user.save((error, user) => {
-        if (error) {
-            return res.status(400).json({
-                error: "Khoong the them"
-            })
-        }
-        user.salt = undefined
-        user.hashed_password = undefined
-        res.json(user)
-    })
+  const user = new User(req.body);
+  user.save((error, user) => {
+    if (error) {
+      return res.status(400).json({
+        error: "Khoong the them",
+      });
+    }
+    user.salt = undefined;
+    user.hashed_password = undefined;
+    res.json(user);
+  });
 };
 export const signin = (req, res) => {
-    const { email, password } = req.body;
-    User.findOne({ email }, (error, user) => {
-        if (error || !user) {
-            return res.status(400).json({
-                error: 'Email cua ban khong hop le . Vui long dang ki'
-            })
-        }
+  const { email, password, phone } = req.body;
+  User.findOne({ email } || { phone }, (error, user) => {
+    if (error || !user) {
+      return res.status(400).json({
+        error:
+          "Email hoặc số điện thoại của bạn không hợp lệ . Vui lòng đăng ký",
+      });
+    }
 
-        if (!user.authenticate(password)) {
-            return res.status(401).json({
-                error: 'Email va Password cua ban khong dung'
-            })
-        }
+    if (!user.authenticate(password)) {
+      return res.status(401).json({
+        error: "Email và Password của bạn không đúng ?",
+      });
+    }
 
-        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
-        res.cookie('t', token, { expire: new Date() + 9999 });
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+    res.cookie("t", token, { expire: new Date() + 9999 });
 
-        const { _id, name, avatar, email, role } = user;
-        return res.json({
-            token,
-            user: { _id, avatar, email, name, role }
-        })
-    })
+    const { _id, name, avatar, email, role, phone } = user;
+    return res.json({
+      token,
+      user: { _id, avatar, email, name, role, phone },
+    });
+  });
 };
 
-
 export const signout = (req, res) => {
-    res.clearCookie('t');
-    res.json({
-        message: 'Đăng xuất thành công'
-    })
-}
+  res.clearCookie("t");
+  res.json({
+    message: "Đăng xuất thành công",
+  });
+};
 export const requireSignin = expressJwt({
-    secret: process.env.JWT_SECRET,
-    algorithms: ["HS256"], // added later
-    userProperty: "auth",
+  secret: process.env.JWT_SECRET,
+  algorithms: ["HS256"], // added later
+  userProperty: "auth",
 });
 
 export const isAuth = (req, res, next) => {
-    let user = req.profile && req.auth && req.profile._id == req.auth._id;
-    if (!user) {
-        return res.status(403).json({
-            error: "Quyền truy cập bị Từ chối"
-        })
-    }
-    next();
-}
+  let user = req.profile && req.auth && req.profile._id == req.auth._id;
+  if (!user) {
+    return res.status(403).json({
+      error: "Quyền truy cập bị Từ chối",
+    });
+  }
+  next();
+};
 
 export const isAdmin = (req, res, next) => {
-    if (req.profile.role == 0) {
-        return res.status(403).json({
-            error: "Admin resource! Access Denined"
-        })
-    }
-    next();
-}
+  if (req.profile.role == 0) {
+    return res.status(403).json({
+      error: "Admin resource! Access Denined",
+    });
+  }
+  next();
+};
--- modoles/Users.js
+++ modoles/Users.js
@@ -1,70 +1,73 @@
-const mongoose = require('mongoose');
-const crypto = require('crypto');
-const { v1: uuidv1 } = require('uuid');
+const mongoose = require("mongoose");
+const crypto = require("crypto");
+const { v1: uuidv1 } = require("uuid");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        trim: true,
-      
+      type: String,
+      trim: true,
     },
     email: {
-        type: String,
-        trim: true,
-        
+      type: String,
+      trim: true,
     },
     hashed_password: {
-        type: String,
+      type: String,
     },
     about: {
-        type: String,
-        trim: true,
+      type: String,
+      trim: true,
+    },
+    phone: {
+      type: String,
     },
     avatar: {
-        type: String
+      type: String,
     },
     salt: {
-        type: String
+      type: String,
     },
     role: {
-        type: Number,
-        default: 0
+      type: Number,
+      default: 0,
     },
     history: {
-        type: Array,
-        default: []
+      type: Array,
+      default: [],
     },
     // password: {
     //     type: String,
     //     required: true
     // }
-
-}, { timestamps: true })
-
+  },
+  { timestamps: true }
+);
 
 // vitual field
 
-userSchema.virtual('password') // Tạo 1 field ảo
-    .set(function(password) {
-        this.salt = uuidv1() //unique
-        this.hashed_password = this.encrytPassword(password)
-    })
+userSchema
+  .virtual("password") // Tạo 1 field ảo
+  .set(function (password) {
+    this.salt = uuidv1(); //unique
+    this.hashed_password = this.encrytPassword(password);
+  });
 
 userSchema.methods = {
-    authenticate: function(plainText) {
-        return this.encrytPassword(plainText) === this.hashed_password;
-    },
-    encrytPassword: function(password) {
-        if (!password) return '';
-        try {
-            return crypto
-                .createHmac('sha1', this.salt)
-                .update(password)
-                .digest('hex')
-        } catch (error) {
-            return "";
-        }
+  authenticate: function (plainText) {
+    return this.encrytPassword(plainText) === this.hashed_password;
+  },
+  encrytPassword: function (password) {
+    if (!password) return "";
+    try {
+      return crypto
+        .createHmac("sha1", this.salt)
+        .update(password)
+        .digest("hex");
+    } catch (error) {
+      return "";
     }
-}
+  },
+};
 
 module.exports = mongoose.model("user", userSchema);
